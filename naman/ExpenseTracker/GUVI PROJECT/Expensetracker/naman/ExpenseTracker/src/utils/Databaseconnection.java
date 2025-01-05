e63880fdf6c7ac00cfb6d22046fd4d972ea023f4
Ashutosh Kumar Jha <jhaash925@gmail.com>
2025-01-05 17:31:40 +0530

    Delete naman/ExpenseTracker/GUVI PROJECT/Expensetracker/naman/ExpenseTracker/src directory

--- naman/ExpenseTracker/GUVI PROJECT/Expensetracker/naman/ExpenseTracker/src/utils/Databaseconnection.java
+++
@@ -1,8 +0,0 @@
-public static Connection connectToDatabase() {
-    try {
-        return DriverManager.getConnection(DB_URL);
-    } catch (SQLException e) {
-        System.err.println("Error connecting to database: " + e.getMessage());
-        return null;
-    }
-}
